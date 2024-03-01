b0623e4bae3fa9d00323f3bd5a70fa42814b9fa2
MichalBaron <MBaron@trueblue.com>
2024-03-01 19:29:51 +0100

    Init commit

---
+++ ms_teams_notification/tests/unit/test_ms_teams_notification_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from ms_teams_notification.ms_teams_notification_stack import MsTeamsNotificationStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ms_teams_notification/ms_teams_notification_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = MsTeamsNotificationStack(app, "ms-teams-notification")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
