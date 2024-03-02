9ae6c07e52093f5dfa3e9d620a7a5ab058a5b711
MichalBaron <MBaron@trueblue.com>
2024-03-02 17:43:22 +0100

    sns topic

--- ms_teams_notification/ms_teams_notification_infrastructure/ms_teams_notification_stack.py
+++ ms_teams_notification/ms_teams_notification_infrastructure/ms_teams_notification_stack.py
@@ -1,9 +1,9 @@
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_cloudwatch as cloudwatch,
+    aws_cloudwatch_actions as cw_actions,
     Duration,
-    # aws_sqs as sqs,
+    aws_sns as sns,
 )
 from constructs import Construct
 
@@ -12,12 +12,16 @@ class MsTeamsNotificationStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.create_cloudwatch_alarm(namespace="AWS/Lambda", metric_name="Invocations")
+        sns_topic = self.create_sns_topic()
+        self.create_cloudwatch_alarm(
+            namespace="AWS/Lambda", metric_name="Invocations", sns_topic=sns_topic
+        )
 
     def create_cloudwatch_alarm(
         self,
         namespace: str,
         metric_name: str,
+        sns_topic: sns.Topic,
         period: Duration = Duration.minutes(5),
         threshold: int = 100,
         evaluation_periods: int = 1,
@@ -42,8 +46,16 @@ class MsTeamsNotificationStack(Stack):
             treat_missing_data=missing_data_treatment,
         )
 
-    def create_sns_topic(self):
-        ...
+        sns_action = cw_actions.SnsAction(sns_topic)
+        alarm.add_alarm_action(sns_action)
+
+    def create_sns_topic(self) -> sns.Topic:
+        return sns.Topic(
+            self,
+            "DemoCloudWatchNotificationSnsTopic",
+            topic_name=f"demo-cloud-watch-notifications",
+            display_name="Demo cloud watch Notifications",
+        )
 
     def create_notification_lambda(self):
         ...
